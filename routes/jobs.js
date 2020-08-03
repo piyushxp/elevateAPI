5c1f05d8345b962d20195c3ee05a1b72e39afdb0
piyush-mahapatra-au6 <piyushmahapatra001@gmail.com>
2020-08-04 02:18:52 +0530

    Need to fix the text-index-query thing tomorrow,using native mongo shell

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,8 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getJobs, newJob } = require('../controllers/jobsController');
+const {
+	getJobs,
+	newJob,
+	getJobsInRadius,
+    updateJob,
+    deleteJob,getJob
+} = require("../controllers/jobsController");
 
-router.route('/jobs').get(getJobs);
-router.route('jobs/new').post(newJob);
+router.route("/jobs").get(getJobs);
+router.route("/job/new").post(newJob);
+router.route("/jobs/:zipcode/:distance").get(getJobsInRadius);
+router.route("/jobs/:id").put(updateJob);
+router.route("/jobs/:id").delete(deleteJob);
+router.route("/job/:id/:slug").get(getJob);
+
 
 module.exports = router;
